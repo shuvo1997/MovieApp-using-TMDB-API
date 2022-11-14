18b80be082d88c8d5504aa30694d7dd56b47ceb3
Shovon Karmaker <shuvo1997@yahoo.com>
2022-11-14 12:45:58 +0600

    Session timeout simplified

--- MovieApp/ViewModel/AuthTokenViewModel.swift
+++ MovieApp/ViewModel/AuthTokenViewModel.swift
@@ -8,18 +8,19 @@
 import Foundation
 
 class AuthTokenViewModel: ObservableObject {
-   var time = 0
-
+//    var time = 0
+//    let timeOut = 30
     
-    func checkExpired(currentTime: Int) -> Bool{
-        print("Current time\(currentTime)")
-        if currentTime - time >= 300 {
-           return true
-        }
-        else {
-            return false
-        }
-    }
+    
+//    func checkExpired(currentTime: Int) -> Bool{
+//        print("Current time\(currentTime)")
+//        if currentTime - time >= 30 {
+//            return true
+//        }
+//        else {
+//            return false
+//        }
+//    }
     
     func saveToken() {
         let token = "dummy-api-token"
@@ -28,26 +29,26 @@ class AuthTokenViewModel: ObservableObject {
         
         do {
             try KeychainManager.save(service: service, account: account, data: token.data(using: .utf8) ?? Data())
-            time = getTime()
-            print("Started time \(time)")
+//            time = getTime()
+//            print("Started time \(time)")
         }
         catch {
             print(error)
         }
     }
-    
-    func getTime()->Int{
-        let time = Date()
-        let timeFormatter = DateFormatter()
-        timeFormatter.dateFormat = "ss"
-        let stringDate = timeFormatter.string(from: time)
-        return Int(stringDate) ?? 0
-    }
+//
+//    func getTime()->Int{
+//        let time = Date()
+//        let timeFormatter = DateFormatter()
+//        timeFormatter.dateFormat = "ss"
+//        let stringDate = timeFormatter.string(from: time)
+//        return Int(stringDate) ?? 0
+//    }
     
     func deleteToken() {
         let service = "api-token"
         KeychainManager.delete(service: service)
-        time = 0
+//        time = 0
     }
     
 }
